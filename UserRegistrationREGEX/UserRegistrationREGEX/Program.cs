d4c8293f63c87c3de3d241b0d6335046147c0b07
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-04-25 15:55:11 +0530

    UC1 : Enter Valid First Name

--- UserRegistrationREGEX/UserRegistrationREGEX/Program.cs
+++ UserRegistrationREGEX/UserRegistrationREGEX/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inStr = Console.ReadLine();
+
+            string firstName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
+
+            Regex regex = new Regex(firstName);
+            Console.WriteLine(regex.IsMatch(inStr));
+        }
+    }
+}
